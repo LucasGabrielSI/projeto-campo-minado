b381c59a92e55a5236191878bdacbac5ea0282cb
Lucas Gabriel <lucasiguape16@gmail.com>
2018-09-04 15:36:57 -0300

    classe para manipulacao de arquivos

--- manipulacao_arquivo.py
+++ manipulacao_arquivo.py
@@ -1,15 +1,15 @@
 import os
-import shutil 
+import shutil
 from os import path
 
 class ManipulaArquivo:
     def __init__(self, path, tipo):
         self.path = path
-        self.tipo = tipo 
+        self.tipo = tipo
 
     def openArchive(self):
         open(self.path, self.tipo)
-        
+
 
 if __name__ == "__main__":
     arquivo = ManipulaArquivo('teste.txt', 'w')
